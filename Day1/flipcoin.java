7a7e01e6c5f9710417a11dc954d01087b6a9fe77
Priyanka <priyankaag.0613@gmail.com>
2023-12-19 20:01:19 +0530

    changes for day1

--- Day1/flipcoin.java
+++ Day1/flipcoin.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class flipCoin {
+public class flipcoin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of times to flip the coin: ");
