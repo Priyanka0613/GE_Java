ca12e910dfed6a19cc3f55b36a2bcda806233498
Priyanka0613 <95872454+Priyanka0613@users.noreply.github.com>
2023-12-19 06:27:27 +0530

    Create leapYear.java
    2. Leap Year
    a. I/P -> Year, ensure it is a 4-digit number.
    b. Logic -> Determine if it is a Leap Year.
    c. O/P -> Print the year as a Leap Year or not.

---
+++ leapYear.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LeapYearChecker {
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year (4 digits): ");
+        int year = scanner.nextInt();
+
+        if (year >= 1000 && year <= 9999) {
+            if (isLeapYear(year)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        } else {
+            System.out.println("Enter a 4-digit year.");
+        }
+    }
+}
